5522bd9072133e85953b50f1b42ecad4aa5f7128
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-07-19 06:46:00 -0500

    adjust minigame

--- our_code.py
+++ our_code.py
@@ -6,11 +6,9 @@ import os
 import keyboard
 import random
 
-global_down: bool = False
 
 def get_screen(title:str, target:tuple = None, mini: bool = False) -> list[object, tuple]:
     ao: object = pyautogui.getWindowsWithTitle(title) #'Albion Online Client'
-    # print(f'active {pyautogui.getActiveWindow()}')
     if(ao):
         while (pyautogui.getActiveWindow() != ao[0]):
             print('Window is closed')
@@ -22,20 +20,13 @@ def get_screen(title:str, target:tuple = None, mini: bool = False) -> list[objec
         active_region: tuple = (ao[0].left, ao[0].top, ao[0].right, ao[0].bottom)
         if(mini):
             target_region: tuple = minibox(active_region)
-            # print('mini')
         else:
             if(target is None):
                 target_region: tuple = active_region
-                # print('NO TARGET')
             else:
                 target_region:tuple = find_quarter(active_region, target)
-                # print('Target')
-        
-    
-        # print(active_region, target_region)
         screenshot: object = ImageGrab.grab(bbox=target_region, all_screens=True)
         return screenshot, target_region
-        # print(active_window_region)
     else:
         return None, None
 
@@ -46,14 +37,13 @@ def minibox(active_region: tuple) -> tuple:
     # Width and height of active window
     width: int = int(active_region[2] - active_region[0])
     height: int = int(active_region[3] - active_region[1])
-    # 0.1 = 10% and 0.2 = 20% for height and width detection box in minigame, might need to adjust for dif resolution of game screen
+    # 0.2 = 20% and 0.3 = 30% for height and width detection box in minigame, might need to adjust for dif resolution of game screen
     return mid_x - int(width * 0.2),mid_y - int(height * 0.3), mid_x + int(width * 0.2), mid_y + int(height * 0.3)
 
 def find_quarter(active_region: tuple, target: tuple) -> tuple:
     mid_x: int = int((active_region[0]+active_region[2]) / 2)
     mid_yy: int = int((active_region[1]+active_region[3]) / 2)
     mid_y: int = int(mid_yy-(mid_yy*0.038)) # Adjusting mid_Y by 3.8%
-    # print(f"MID_Y:{mid_y}")
     if(target[0]>mid_x and target[1]<mid_y):
         # print('TOP_RIGHT')
         return mid_x, active_region[1],active_region[2],mid_y
@@ -100,33 +90,18 @@ def exiting() -> None:
 
 
 def throw(x, y) -> None:
-    # pyautogui.moveTo(x)
-    global global_down
     pyautogui.mouseDown(x=x, y=y)
-    global_down = True
     time.sleep(random.randint(2, 10)/10)
     pyautogui.mouseUp()
-    global_down = False
 
 def cork_loc(x, reg):
     detect_box_width = reg[2]-reg[0]
-    # print(f'xlen = {x_len}')
-    # print(f'x_сork = {x}')
-    global global_down
-    # 70 % of detection box, since we adjusted detection box for mini game we getting back x relevant that detection box
-    # pyautogui.mouseDown() # Test1 Start
-    if (int(x)>(detect_box_width*0.7)): 
+    # 55 % of detection box, since we adjusted detection box for mini game we getting back Avg(x) relevant that detection box
+    if (int(x)>(detect_box_width*0.55)): 
         pyautogui.mouseUp() 
     else:
         pyautogui.mouseDown()
-    # Test1 End
-    # if(int(x)<(detect_box_width*0.57)): 
-    #     print(f'cork location {x} and width of detection box {detect_box_width}')
-    #     pyautogui.mouseDown()
-    #     global_down = True
-    # else: 
-    #     pyautogui.mouseUp()
-    #     global_down = False
+  
     
 
 
@@ -134,15 +109,12 @@ def minigame(title: str) -> None:
     detections, reg = detection(title, None, True)
     vals = iterate_df(detections) 
     could_not: int = 0
-    global global_down
-    # print(f'Vals before while{vals}')
     while(could_not < 5):
         if('minigame' not in vals):
             if('cork' not in vals):
                 could_not+=1
                 print(f'No minigame or cork, couldnot find = {could_not}')                
         else:
-        # while('minigame' in vals or 'cork' in vals):
             if(keyboard.is_pressed('k')):
                 print('Exited cycle')
                 return
@@ -151,18 +123,14 @@ def minigame(title: str) -> None:
                 for index, row in detections.iterrows():
                     if (row['name'] == 'cork'):
                         x = (row["xmin"]+row["xmax"])/2
-                        pyautogui.mouseDown #Test1
+                        pyautogui.mouseDown # Mouse down when minigame starts
                         cork_loc(x, reg)
             detections, reg = detection(title, None, True)
             if isinstance(detections, list):
                 pyautogui.mouseUp()
-                global_down = False
                 time.sleep(1)
                 return                
-            # print(f'Detections: /n {detections}')
-            vals = iterate_df(detections)
-            # print(f'Detections: /n {vals}')
-        # print(vals)       
+            vals = iterate_df(detections)      
         detections, reg = detection(title, None, True)
         vals = iterate_df(detections)
 
@@ -175,22 +143,16 @@ def iterate_df(df) -> list:
 
 
 def runner(x,y,title) -> None:
-    # print(f"Target:{x,y}")
-    global global_down
     target: tuple = (x,y)
     throw(x, y)
     time.sleep(2)
     detections , region = detection(title, target)
-    # print(f'Region:{region}')
     vals: list = iterate_df(detections)
     
     while('stat' in vals):
         if(keyboard.is_pressed('k')):
             print('Exited cycle')
             return
-        # print('detected stat')
-        # time.sleep(0.2)
-        # print(detections)
         if('catch' in vals):
             break
         detections, region  = detection(title, target)
@@ -200,18 +162,10 @@ def runner(x,y,title) -> None:
                 if (row['name'] == 'catch'):
                     if (row['confidence'] > 0.7):
                         print(f"Confidence = {row['confidence']}")
-                        pyautogui.click(x, y)
-                        # if (pyautogui.position() != x,y):
-                        #     pyautogui.moveTo(x,y)
-                        # pyautogui.mouseDown()
-                        # global_down = True
-                        # time.sleep(0.2)    
+                        pyautogui.click(x, y) 
                         minigame(title)
-                        # print(f'Exited minigame. Mouse down: {global_down}')
                         pyautogui.mouseUp()
-                        global_down = False
                         time.sleep(0.5)              
-    # time.sleep(0.2)
 
 
 def start() -> None:
@@ -225,9 +179,7 @@ def start() -> None:
 
 def mini() -> None:
     title: str = 'Albion Online Client'
-    global global_down
     pyautogui.mouseDown()
-    global_down = True
     time.sleep(0.1)
     minigame(title)
     x, y = pyautogui.position()
